76a5209bd57d7924980d6f8f923cf26f0267b99a
Phil Hanna <ph1204@gmail.com>
2023-11-12 14:40:13 -0500

    Added comments in doc.go

--- doc.go
+++ doc.go
@@ -3,8 +3,24 @@ package tree
 
 // Package-global variables
 var (
+
+	// FlagA is the value of the -a command line option. If true, then
+	// the tree includes hidden files.
 	FlagA bool
+
+	// FlagL is the value of the -L command line option. If specified,
+	// limits the depth of the subdirectories included.
 	FlagL int
+
+	// FlagNoReport is the value of the --noreport command line option.
+	// If true, the program does not print the number of directories and
+	// files at the end of the tree.
+	FlagNoReport bool
+
+	// NFiles is used to keep track of how many files have been printed.
 	NFiles int
+
+	// NDirs is used to keep track of how many directories have been
+	// printed.
 	NDirs int
 )
