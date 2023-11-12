5e431ca57f250b80499d8d5694ebf4b686a64318
Phil Hanna <ph1204@gmail.com>
2023-11-12 08:13:12 -0500

    Finished TreeString()

--- inode.go
+++ inode.go
@@ -11,3 +11,32 @@ type INode interface {
 	GetLevel() int   // How far removed from root node
 	IsLast() bool    // True if this is the last child of the parent
 }
+
+// ---------------------------------------------------------------------
+// Functions
+// ---------------------------------------------------------------------
+
+// TreeString creates a string representation of this node
+func TreeString(node INode) string {
+	var prefix string
+	if node.GetLevel() > 0 {
+		switch node.IsLast() {
+		case false:
+			prefix = "├───"
+		case true:
+			prefix = "└───"
+		}
+	}
+	work := node
+	for work.GetParent() != nil {
+		parent := work.GetParent()
+		switch parent.IsLast() {
+		case false:
+			prefix = "│   " + prefix
+		case true:
+			prefix = "└   " + prefix
+		}
+	}
+	line := prefix + node.GetName()
+	return line
+}
